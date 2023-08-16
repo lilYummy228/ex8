cf17d7dd35ab27788384b906b40f5910e94f0dda
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-08-16 12:03:06 +0500

    Добавьте файлы проекта.

---
+++ ex8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exitWord = true;
+            string word;
+
+            while (exitWord)
+            {
+                Console.Write("Введите слово exit: ");
+                word = Console.ReadLine();
+
+                if (word == "exit")
+                {
+                    exitWord = false;
+                }
+                else
+                {
+                    Console.WriteLine("Неверное слово!");
+                }
+            }
+        }
+    }
+}
